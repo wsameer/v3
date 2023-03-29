a348d2ca53ca19319ce1a4426e421097e17b2881
wsameer <dev.wsameer@gmail.com>
2023-03-28 23:12:06 -0400

    build: adds absolute path support

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig((configEnv) => {
     plugins: [react()],
     resolve: {
       alias: {
-        app: resolve(__dirname, "src", "app"),
         components: resolve(__dirname, "src", "components"),
         hooks: resolve(__dirname, "src", "hooks"),
       },
