fa00fdffe6de4e154e7e92efbe12fae0e2f44569
wsameer <dev.wsameer@gmail.com>
2023-04-01 15:22:22 -0400

    feat: adds generic Text component

---
+++ src/components/common/Text/Text.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { TextProps } from './types';
+
+export const Text: React.FC<TextProps> = React.memo(
+    ({ className, children }) => {
+        return <p className={`${className}`}>{children}</p>;
+    }
+);
+
+Text.displayName = 'Text';
