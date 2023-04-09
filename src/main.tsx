b1d4e2b31d36870e0d7c32b207db88943241a88f
wsameer <dev.wsameer@gmail.com>
2023-04-09 09:41:56 -0400

    removes strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import App from '@components/App';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<HelmetProvider>
-			<App />
-		</HelmetProvider>
-	</React.StrictMode>
+	<HelmetProvider>
+		<App />
+	</HelmetProvider>
 );
