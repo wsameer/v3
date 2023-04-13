70d971e55a02d7d5510b16d3477f78df3e59598f
wsameer <dev.wsameer@gmail.com>
2023-04-12 23:25:03 -0400

    refactor: removes unused imports

--- src/components/common/Button/types.ts
+++ src/components/common/Button/types.ts
@@ -1,9 +1,7 @@
-import type { HTMLProps } from 'react';
-
-export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
-	as?: any;
-	to?: string;
-	variant?: 'solid' | 'flat' | 'outlined';
-	disabled?: boolean;
-	type?: 'button' | 'reset' | 'submit';
+export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  as?: any;
+  to?: string;
+  variant?: "solid" | "flat" | "outlined";
+  disabled?: boolean;
+  type?: "button" | "reset" | "submit";
 }
