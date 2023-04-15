b84a7f66dd9e67c39a5a1742e008434316353226
wsameer <dev.wsameer@gmail.com>
2023-04-15 09:56:46 -0400

    removes unsused imports

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import Routes from "@components/Routes/Routes";
+
+const App: React.FC = () => (
+  <div className="min-h-screen bg-gray-700 md:p-6">
+    <Routes />
+  </div>
+)
+
+export default App;
