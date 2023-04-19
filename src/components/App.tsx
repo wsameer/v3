3aae865616ca51b4c5ec819e209ee7267378b049
wsameer <dev.wsameer@gmail.com>
2023-04-18 21:34:09 -0400

    updates the layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 import Routes from "@components/Routes/Routes";
+import { Footer } from "./Footer/Footer";
+import { Header } from "./layout/Header/Header";
 
 const App: React.FC = () => (
   <div className="min-h-screen bg-gray-700 md:p-6">
+    <Header />
     <Routes />
+    <Footer />
   </div>
-)
+);
 
 export default App;
