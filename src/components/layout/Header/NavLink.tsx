770bc1c895fa6050cdb38aa3a6692c56a1fa5a7f
wsameer <dev.wsameer@gmail.com>
2023-04-19 22:48:18 -0400

    replaces <a> tag with Link from react router

--- src/components/layout/Header/NavLink.tsx
+++ src/components/layout/Header/NavLink.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export type NavLinkProps = {
-  title: string;
-  link: string;
-  isActive: boolean;
+	title: string;
+	link: string;
+	isActive: boolean;
 };
 
 const NavLink: React.FC<NavLinkProps> = React.memo(
-  ({ title, link, isActive }) => {
-    return (
-      <li>
-        <a
-          className={`relative block mx-1.5 px-4 py-1.5 text-base rounded-full transition hover:text-purple-500 dark:hover:text-purple-400 ${
-            isActive && "bg-gray-700"
-          }`}
-          href={link}
-        >
-          {title}
-        </a>
-      </li>
-    );
-  }
+	({ title, link, isActive }) => {
+		return (
+			<li>
+				<Link
+					to={link}
+					className={`relative block mx-1.5 px-4 py-1.5 text-base rounded-full transition hover:text-purple-500 dark:hover:text-purple-400 ${
+						isActive && 'bg-gray-700'
+					}`}>
+					{title}
+				</Link>
+			</li>
+		);
+	}
 );
 
 NavLink.propTypes = {
-  title: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  isActive: PropTypes.bool.isRequired,
+	title: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+	isActive: PropTypes.bool.isRequired
 };
 
 NavLink.defaultProps = {
-  title: "",
-  link: "/404",
-  isActive: false,
+	title: '',
+	link: '/404',
+	isActive: false
 };
 
 export default NavLink;
