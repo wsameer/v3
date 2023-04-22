2c80b466a73e2d584b0b09e25663766b2b549c8c
wsameer <dev.wsameer@gmail.com>
2023-04-22 00:35:12 -0400

    basic changes in Testimonials page

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export const Testimonials = () => {
-    return (
-        <div>Testimonials</div>
-    )
+	return <div>Testimonials</div>;
 };
