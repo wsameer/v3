bf086f10f8d1b2e6ab1076c48f8aeb2bc3f8afa9
wsameer <dev.wsameer@gmail.com>
2023-04-22 00:36:05 -0400

    refactors PageLayout component

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,14 +1,16 @@
-import React, { HTMLProps } from "react";
+import React, { HTMLProps } from 'react';
 
 const PageLayout: React.FC<HTMLProps<HTMLDivElement>> = React.memo(
-  ({ children }) => {
-    return (
-      <div id="PageLayout" className="max-w mx-auto overflow-hidden lg:px-12">
-        <div className="relative md:flex">{children}</div>
-      </div>
-    );
-  }
+	({ children }) => {
+		return (
+			<div
+				id="PageLayout"
+				className="max-w mx-auto overflow-hidden lg:px-12">
+				<div className="relative md:flex">{children}</div>
+			</div>
+		);
+	}
 );
 
-PageLayout.displayName = "PageLayout";
+PageLayout.displayName = 'PageLayout';
 export default PageLayout;
