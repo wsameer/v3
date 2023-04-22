d9dfd5dab7d2baa885c10e0a9888e6df2d6c3077
wsameer <dev.wsameer@gmail.com>
2023-04-22 00:35:46 -0400

    types in Button component

--- src/components/common/Button/types.ts
+++ src/components/common/Button/types.ts
@@ -1,7 +1,7 @@
-export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
-  as?: any;
-  to?: string;
-  variant?: "solid" | "flat" | "outlined";
-  disabled?: boolean;
-  type?: "button" | "reset" | "submit";
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+	as?: any;
+	to?: string;
+	variant?: 'solid' | 'flat' | 'outlined';
+	disabled?: boolean;
+	type?: 'button' | 'reset' | 'submit';
 }
