f9a054c402adf8187a7fb8364cf6c64df0a736b8
wsameer <dev.wsameer@gmail.com>
2023-04-26 21:28:59 -0400

    moved header and footer out of App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,9 @@
-import Routes from '@components/Routes/Routes';
-import { Footer } from './Footer/Footer';
-import { Header } from './layout/Header/Header';
+import Routes from "@components/Routes/Routes";
 
 const App: React.FC = () => (
-	<div className="min-h-screen bg-gray-700 md:p-6">
-		<Header />
-		<Routes />
-		<Footer />
-	</div>
+  <div className="min-h-screen bg-gray-700">
+    <Routes />
+  </div>
 );
 
 export default App;
